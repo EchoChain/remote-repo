3f804a1192d649e6084bf22fd7dc1110387a5866
Cheng Yihao <2586110419@qq.com>
2024-08-27 22:06:56 +0800

    main:1

---
+++ Main.java
@@ -0,0 +1,11 @@
+/**
+ * @author Cheng Yihao
+ * @version 1.0
+ * @date 2024/8/27 22:02
+ * @comment
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
